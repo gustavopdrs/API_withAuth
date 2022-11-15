8ab6e5d2df737ecd61fbc96a869b37c1fa6ad263
Gustavo Pedroso Gal <gustavopgal@gmail.com>
2022-11-14 22:39:26 -0300

    test

--- routes/auth.js
+++ routes/auth.js
@@ -72,50 +72,19 @@ passport.deserializeUser(function(user, cb) {
   });
 });
 
-
+const controleElencos = require('../controladores/elencos');
+const seguranca = require('../controladores/seguranca')
+const controleJogadores = require("../controladores/jogadores");
 var router = express.Router();
-
-/* GET /login
- *
- * This route prompts the user to log in.
- *
- * The 'login' view renders an HTML page, which contain a button prompting the
- * user to sign in with Google.  When the user clicks this button, a request
- * will be sent to the `GET /login/federated/accounts.google.com` route.
- */
 router.get('/login', function(req, res, next) {
   res.render('login');
 });
-
-/* GET /login/federated/accounts.google.com
- *
- * This route redirects the user to Google, where they will authenticate.
- *
- * Signing in with Google is implemented using OAuth 2.0.  This route initiates
- * an OAuth 2.0 flow by redirecting the user to Google's identity server at
- * 'https://accounts.google.com'.  Once there, Google will authenticate the user
- * and obtain their consent to release identity information to this app.
- *
- * Once Google has completed their interaction with the user, the user will be
- * redirected back to the app at `GET /oauth2/redirect/accounts.google.com`.
- */
 router.get('/login/federated/google', passport.authenticate('google'));
-
-/*
-    This route completes the authentication sequence when Google redirects the
-    user back to the application.  When a new user signs in, a user account is
-    automatically created and their Google account is linked.  When an existing
-    user returns, they are signed in to their linked account.
-*/
 router.get('/oauth2/redirect/google', passport.authenticate('google', {
   successReturnToOrRedirect: '/',
   failureRedirect: '/login'
 }));
 
-/* POST /logout
- *
- * This route logs the user out.
- */
 router.post('/logout', function(req, res, next) {
   req.logout(function(err) {
     if (err) { return next(err); }
@@ -123,4 +92,24 @@ router.post('/logout', function(req, res, next) {
   });
 });
 
+router.route('/elencos')
+     .get(seguranca.verificaJWT, controleElencos.getElencos)
+     .post(seguranca.verificaJWT, controleElencos.addElenco)
+     .put(seguranca.verificaJWT, controleElencos.updateElenco)
+
+router.route('/elencos/:codigo')
+     .get(seguranca.verificaJWT, controleElencos.getElencoPorCodigo)
+     .delete(seguranca.verificaJWT, controleElencos.deleteElenco)
+
+router.route('/jogadores')
+   .get(seguranca.verificaJWT, controleJogadores.getJogadores)
+   .post(seguranca.verificaJWT, controleJogadores.addJogador)
+   .put(seguranca.verificaJWT, controleJogadores.updateJogador)
+
+router.route('/jogadores/:codigo')
+   .get(seguranca.verificaJWT, controleJogadores.getJogadorPorCodigo)
+   .delete(seguranca.verificaJWT, controleJogadores.deleteJogador)
+
+router.route("/login")
+   .post(seguranca.login) 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,122 +1,40 @@
-var express = require('express');
+const { Router } = require('express');
+const controleJogadores = require("../controladores/jogadores");
+const controleElencos = require('../controladores/elencos');
 var ensureLogIn = require('connect-ensure-login').ensureLoggedIn;
 var db = require('../db');
 
 var ensureLoggedIn = ensureLogIn();
 
-function fetchTodos(req, res, next) {
-  db.all('SELECT * FROM todos WHERE owner_id = ?', [
-    req.user.id
-  ], function(err, rows) {
-    if (err) { return next(err); }
-    
-    var todos = rows.map(function(row) {
-      return {
-        id: row.id,
-        title: row.title,
-        completed: row.completed == 1 ? true : false,
-        url: '/' + row.id
-      }
-    });
-    res.locals.todos = todos;
-    res.locals.activeCount = todos.filter(function(todo) { return !todo.completed; }).length;
-    res.locals.completedCount = todos.length - res.locals.activeCount;
-    next();
-  });
-}
 
-var router = express.Router();
+const rotas = new Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+rotas.get('/', function(req, res, next) {
   if (!req.user) { return res.render('home'); }
   next();
-}, fetchTodos, function(req, res, next) {
+}, function(req, res, next) {
   res.locals.filter = null;
   res.render('index', { user: req.user });
 });
 
-router.get('/active', ensureLoggedIn, fetchTodos, function(req, res, next) {
-  res.locals.todos = res.locals.todos.filter(function(todo) { return !todo.completed; });
-  res.locals.filter = 'active';
-  res.render('index', { user: req.user });
-});
+rotas.route('/elencos')
+     .get(ensureLoggedIn, controleElencos.getElencos)
+     .post(ensureLoggedIn, controleElencos.addElenco)
+     .put(ensureLoggedIn, controleElencos.updateElenco)
 
-router.get('/completed', ensureLoggedIn, fetchTodos, function(req, res, next) {
-  res.locals.todos = res.locals.todos.filter(function(todo) { return todo.completed; });
-  res.locals.filter = 'completed';
-  res.render('index', { user: req.user });
-});
+rotas.route('/elencos/:codigo')
+     .get(ensureLoggedIn, controleElencos.getElencoPorCodigo)
+     .delete(ensureLoggedIn, controleElencos.deleteElenco)
 
-router.post('/', ensureLoggedIn, function(req, res, next) {
-  req.body.title = req.body.title.trim();
-  next();
-}, function(req, res, next) {
-  if (req.body.title !== '') { return next(); }
-  return res.redirect('/' + (req.body.filter || ''));
-}, function(req, res, next) {
-  db.run('INSERT INTO todos (owner_id, title, completed) VALUES (?, ?, ?)', [
-    req.user.id,
-    req.body.title,
-    req.body.completed == true ? 1 : null
-  ], function(err) {
-    if (err) { return next(err); }
-    return res.redirect('/' + (req.body.filter || ''));
-  });
-});
+rotas.route('/jogadores')
+   .get(ensureLoggedIn, controleJogadores.getJogadores)
+   .post(ensureLoggedIn, controleJogadores.addJogador)
+   .put(ensureLoggedIn, controleJogadores.updateJogador)
 
-router.post('/:id(\\d+)', ensureLoggedIn, function(req, res, next) {
-  req.body.title = req.body.title.trim();
-  next();
-}, function(req, res, next) {
-  if (req.body.title !== '') { return next(); }
-  db.run('DELETE FROM todos WHERE id = ? AND owner_id = ?', [
-    req.params.id,
-    req.user.id
-  ], function(err) {
-    if (err) { return next(err); }
-    return res.redirect('/' + (req.body.filter || ''));
-  });
-}, function(req, res, next) {
-  db.run('UPDATE todos SET title = ?, completed = ? WHERE id = ? AND owner_id = ?', [
-    req.body.title,
-    req.body.completed !== undefined ? 1 : null,
-    req.params.id,
-    req.user.id
-  ], function(err) {
-    if (err) { return next(err); }
-    return res.redirect('/' + (req.body.filter || ''));
-  });
-});
+rotas.route('/jogadores/:codigo')
+   .get(ensureLoggedIn, controleJogadores.getJogadorPorCodigo)
+   .delete(ensureLoggedIn, controleJogadores.deleteJogador)
 
-router.post('/:id(\\d+)/delete', ensureLoggedIn, function(req, res, next) {
-  db.run('DELETE FROM todos WHERE id = ? AND owner_id = ?', [
-    req.params.id,
-    req.user.id
-  ], function(err) {
-    if (err) { return next(err); }
-    return res.redirect('/' + (req.body.filter || ''));
-  });
-});
 
-router.post('/toggle-all', ensureLoggedIn, function(req, res, next) {
-  db.run('UPDATE todos SET completed = ? WHERE owner_id = ?', [
-    req.body.completed !== undefined ? 1 : null,
-    req.user.id
-  ], function(err) {
-    if (err) { return next(err); }
-    return res.redirect('/' + (req.body.filter || ''));
-  });
-});
-
-router.post('/clear-completed', ensureLoggedIn, function(req, res, next) {
-  db.run('DELETE FROM todos WHERE owner_id = ? AND completed = ?', [
-    req.user.id,
-    1
-  ], function(err) {
-    if (err) { return next(err); }
-    return res.redirect('/' + (req.body.filter || ''));
-  });
-});
-
-module.exports = router;
+module.exports = rotas;
